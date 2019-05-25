ac5510ba5a19cf0817bbcff22725f8cdca5fbfe4
Nico D'Cotta <nicolas.dcotta18@imperial.ac.uk>
2019-05-25 19:37:48 +0100

    Fixed instr_type signatures to become pointers

--- src/decode.h
+++ src/decode.h
@@ -5,11 +5,11 @@
 
 Decoded_Instr decode_instr(uint32_t instr);
 
-Data_Proc_Instr decode_data_proc_instr(uint32_t instr);
-Mul_Instr decode_mul_instr(uint32_t instr);
-Data_Trans_Instr decode_data_trans_instr(uint32_t instr);
-Branch_Instr decode_branch_instr(uint32_t instr);
+Data_Proc_Instr *decode_data_proc_instr(uint32_t instr);
+Mul_Instr *decode_mul_instr(uint32_t instr);
+Data_Trans_Instr *decode_data_trans_instr(uint32_t instr);
+Branch_Instr *decode_branch_instr(uint32_t instr);
 
-instr_type get_instr_type(uint32_t instr);
+instr_type *get_instr_type(uint32_t instr);
 
 #endif /* DECODE_H_ */
