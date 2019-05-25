4882f64feaa58f866e75fe33541724b18889b82d
Nico D'Cotta <nicolas.dcotta18@imperial.ac.uk>
2019-05-25 20:06:21 +0100

    Made memory signatures of instructions structs

--- src/decode.c
+++ src/decode.c
@@ -29,10 +29,14 @@ Decoded_Instr decode_instr(uint32_t instr)
 
 Data_Proc_Instr *decode_data_proc_instr(uint32_t instr)
 {
+  Data_Proc_Instr *ret =
+    malloc(sizeof(Data_Proc_Instr));
+
 }
 
 Mul_Instr *decode_mul_instr(uint32_t instr)
 {
+  return malloc(sizeof(decode_mul_instr));
 }
 
 Data_Trans_Instr *decode_data_trans_instr(uint32_t instr)
@@ -41,6 +45,7 @@ Data_Trans_Instr *decode_data_trans_instr(uint32_t instr)
 
 Branch_Instr *decode_branch_instr(uint32_t instr)
 {
+  return malloc(decode_branch_instr);
 }
 
 instr_type *get_instr_type(uint32_t instr)
--- src/instruction.h
+++ src/instruction.h
@@ -23,18 +23,42 @@ typedef struct s_Decoded_Instr
 
 typedef struct s_Data_Proc_Instr
 {
+  unsigned int cond : 4;
+  unsigned int opcode : 4;
+  unsigned int s : 1;
+  unsigned int rn : 4;
+  unsigned int rd : 4;
+  unsigned int operand_2 : 12;
+
 } Data_Proc_Instr;
 
 typedef struct s_Mul_Instr
 {
+  unsigned int cond : 4;
+  unsigned int a : 1;
+  unsigned int s : 1;
+  unsigned int rd : 4;
+  unsigned int rn : 4;
+  unsigned int rs : 4;
+  unsigned int rm : 4;
 } Mul_Instr;
 
 typedef struct s_Data_Trans_Instr
 {
+  unsigned int cond : 4;
+  unsigned int i : 1;
+  unsigned int p : 1;
+  unsigned int u : 1;
+  unsigned int l : 1;
+  unsigned int rn : 4;
+  unsigned int rd : 4;
+  unsigned int offset: 12;
 } Data_Trans_Instr;
 
 typedef struct s_Branch_Instr
 {
+  unsigned int cond : 4;
+  unsigned int offset : 24;
 } Branch_Instr;
 
 #endif /* INSTRUCTION_H_ */
