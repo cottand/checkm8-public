be5dbda50f0cfbc128d5478f904f4fe94542b603
William PROFIT <williamprofit@dyn3212-189.wlan.ic.ac.uk>
2019-05-25 19:13:27 +0100

    Added base structures for the 4 instr types

---
+++ src/decode.h
@@ -0,0 +1,6 @@
+#ifndef DECODE_H_
+#define DECODE_H_
+
+
+
+#endif /* DECODE_H_ */
--- src/decoded_instr.h
+++ src/decoded_instr.h
@@ -1,13 +1,35 @@
-
-#ifndef DECODED_INSTR
+#ifndef DECODED_INSTR_H_
 #define DECODED_INSTR_H_
 
 #include <stdint.h>
 
+typedef enum e_instr_type
+{
+  Data_Proc,
+  Mul,
+  Data_Trans,
+  Branch
+} instr_type;
+
 typedef struct s_Decoded_Instr
 {
-   /* TODO */
+
 } Decoded_Instr;
 
+typedef struct s_Data_Proc_Instr
+{
+} Data_Proc_Instr;
 
-#endif
+typedef struct s_Mul_Instr
+{
+} Mul_Instr;
+
+typedef struct s_Data_Trans_Instr
+{
+} Data_Trans_Instr;
+
+typedef struct s_Branch_Instr
+{
+} Branch_Instr;
+
+#endif /* DECODED_INSTR_H_ */
