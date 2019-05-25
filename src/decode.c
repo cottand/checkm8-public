465a4b7e9052d48e7f10669d93fdd30677229518
William PROFIT <williamprofit@dyn3212-189.wlan.ic.ac.uk>
2019-05-25 19:31:33 +0100

    Added base implementation of decode function

---
+++ src/decode.c
@@ -0,0 +1,48 @@
+#include "decode.h"
+
+Decoded_Instr decode_instr(uint32_t instr)
+{
+  Decoded_Instr decoded;
+  decoded.type = get_instr_type(instr);
+
+  switch (decoded.type)
+  {
+  case Data_Proc:
+    decoded.data_proc_instr = decode_data_proc_instr(instr);
+    break;
+  case Mul:
+    decoded.mul_instr = decode_mul_instr(instr);
+    break;
+  case Data_Trans:
+    decoded.data_trans_instr = decode_data_trans_instr(instr);
+    break;
+  case Branch:
+    decoded.branch_instr = decode_branch_instr(instr);
+    break;
+  default:
+    printf("Error: invalid instr type");
+    break;
+  }
+
+  return decoded;
+}
+
+Data_Proc_Instr decode_data_proc_instr(uint32_t instr)
+{
+}
+
+Mul_Instr decode_mul_instr(uint32_t instr)
+{
+}
+
+Data_Trans_Instr decode_data_trans_instr(uint32_t instr)
+{
+}
+
+Branch_Instr decode_branch_instr(uint32_t instr)
+{
+}
+
+instr_type get_instr_type(uint32_t instr)
+{
+}
