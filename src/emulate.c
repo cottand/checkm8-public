315b5de1bd8f11afbeaf0da3ac0ed115f46d6695
William PROFIT <williamprofit@dyn3212-189.wlan.ic.ac.uk>
2019-05-26 16:24:16 +0100

    Implemented exec for mul type

--- src/emulate.c
+++ src/emulate.c
@@ -4,8 +4,24 @@
 #include "emulator.h"
 #include "loader.h"
 #include "decode.h"
+#include "execute.h"
 
 int main(int argc, char **argv)
 {
+  Emulator em;
+  emulator_init(&em);
+
+  em.regs[0] = 0x00000001; // Rn
+  em.regs[1] = 0x00000004; // Rs
+  em.regs[2] = 0x00000004; // Rm
+
+  uint32_t instr = 0xe0130192; // No acc
+  //uint32_t instr = 0xe0330192; // acc
+
+  Decoded_Instr decoded = decode_instr(instr);
+  exec_instr(&em, &decoded);
+
+  print_regs(&em);
+
   return EXIT_SUCCESS;
 }
--- src/execute.c
+++ src/execute.c
@@ -28,6 +28,42 @@ void exec_data_proc_instr(Emulator *emulator, Data_Proc_Instr *instr)
 void exec_mul_instr(Emulator *emulator, Mul_Instr *instr)
 {
   if (!is_cond_true(emulator, instr->cond)) { return; }
+
+  uint32_t res = 0;
+  uint32_t rn  = emulator->regs[instr->rn];
+  uint32_t rs  = emulator->regs[instr->rs];
+  uint32_t rm  = emulator->regs[instr->rm];
+  if (instr->a)
+  {
+    res = rm * rs + rn;
+  }
+  else
+  {
+    res = rm * rs;
+  }
+
+  emulator->regs[instr->rd] = res;
+
+  if (instr->s)
+  {
+    if (!res)
+    {
+      set_flag_Z(emulator);
+    }
+    else
+    {
+      clr_flag_Z(emulator);
+    }
+
+    if (get_reg_bit(emulator, instr->rd, 31))
+    {
+      set_flag_N(emulator);
+    }
+    else
+    {
+      clr_flag_N(emulator);
+    }
+  }
 }
 
 void exec_data_trans_instr(Emulator *emulator, Data_Trans_Instr *instr)
