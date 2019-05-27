d8f1982977e6401cbbb6517be473d340393bb6cb
William PROFIT <williamprofit@dyn3212-189.wlan.ic.ac.uk>
2019-05-27 17:01:22 +0100

    Implemented exec for data_trans type (needs further testing)

--- src/emulate.c
+++ src/emulate.c
@@ -10,6 +10,7 @@ int main(int argc, char **argv)
 {
   Emulator em;
   emulator_init(&em);
+  load_binary(&em, argv[1]);
 
   em.regs[0] = 0x00000001; // Rn
   em.regs[1] = 0x00000004; // Rs
@@ -20,12 +21,17 @@ int main(int argc, char **argv)
   //uint32_t instr = 0xe0330192; // acc
 
   // Branch instructions
-  uint32_t instr = 0xea000001;
+  //uint32_t instr = 0xea000001;
+
+  // Data trans instructions
+  //uint32_t instr = 0xe5140000; // Load mem[regs[0]] into regs[4] without offset
+  //uint32_t instr = 0xe5010000; // Store regs[1] into mem[regs[0]] without offset
 
   Decoded_Instr decoded = decode_instr(instr);
   exec_instr(&em, &decoded);
 
   print_regs(&em);
+  print_mem(&em);
 
   return EXIT_SUCCESS;
 }
--- src/execute.c
+++ src/execute.c
@@ -69,6 +69,92 @@ void exec_mul_instr(Emulator *emulator, Mul_Instr *instr)
 void exec_data_trans_instr(Emulator *emulator, Data_Trans_Instr *instr)
 {
   if (!is_cond_true(emulator, instr->cond)) { return; }
+
+  uint32_t offset = 0;
+  if (instr->i) /* Offset is shifted register */
+  {
+    /* The register is obtained with the last 4 bits */
+    uint8_t reg = instr->offset & 0x0f;
+    uint32_t reg_contents = emulator->regs[reg];
+
+    /* Shift type is bits 5 and 6 of offset */
+    uint8_t shift_type    = (instr->offset >> 5) & 0x3;
+    /* Bit 4 determines whether the shift amount is in a reg or is a const */
+    uint8_t use_shift_reg = (instr->offset >> 4) & 0x1;
+
+    uint8_t shift_amount = 0;
+    if (use_shift_reg)
+    {
+      uint8_t shift_reg = (instr->offset >> 8) & 0xf;
+
+      /* Shit amount is last byte of shift reg */
+      shift_amount = emulator->regs[shift_reg];
+    }
+    else
+    {
+      /* Shift amount is bits 7-11 of offset instr offset field */
+      shift_amount = (instr->offset >> 7) & 0x1f;
+    }
+
+    switch (shift_type)
+    {
+    case 0x0: /* Logical left */
+      offset = reg_contents << shift_amount;
+      break;
+    case 0x1: /* Logical right */
+      offset = reg_contents >> shift_amount;
+      break;
+    case 0x2: /* Arithmetic right */
+      if (reg_contents >> 31) /* if the number is negative */
+      {
+        offset = ~(~reg_contents >> shift_amount);
+      }
+      else
+      {
+        offset = reg_contents >> shift_amount;
+      }
+      break;
+    case 0x3: /* Rotate right */
+      if (!(shift_amount &= 31))
+      {
+        offset = reg_contents;
+      }
+      else
+      {
+        offset = (reg_contents >> shift_amount) | (reg_contents << (32 - shift_amount));
+      }
+      break;
+    default:
+      printf("Error: invalid shift type for data trans instr type");
+      return;
+    }
+  }
+  else /* Offset is unsigned 12 bit immediate offset */
+  {
+    offset = instr->offset;
+  }
+
+  uint32_t base = emulator->regs[instr->rn];
+  uint32_t dst  = emulator->regs[instr->rd];
+
+  if (instr->l) /* Load from memory */
+  {
+    base = emulator->mem[dst];
+  }
+  else /* Store to memory */
+  {
+    emulator->mem[dst] = base;
+  }
+
+  /* Only support post indexing */
+  if (!instr->p)
+  {
+    base = instr-> u ? base + offset : base - offset;
+  }
+
+  /* Save the new values into regs */
+  emulator->regs[instr->rn] = base;
+  emulator->regs[instr->rd] = dst;
 }
 
 void exec_branch_instr(Emulator *emulator, Branch_Instr *instr)
