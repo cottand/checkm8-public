e854989d885867335823519638fa372628329c27
Nico D'Cotta <nicolas.dcotta18@imperial.ac.uk>
2019-05-28 16:38:43 +0100

    Fixed forgot to edit pipeline states

--- src/pipeline.c
+++ src/pipeline.c
@@ -14,24 +14,29 @@ void pipeline_init(Pipeline *pipeline, Emulator *emulator)
 
 void cycle_first(Pipeline *pipeline)
 {
-  Emulator* em = pipeline->emulator;
+  Emulator *em = pipeline->emulator;
   pipeline->fetching = fetch(em);
   incr_PC(em);
+  pipeline->current_state = Half;
 }
 
 void cycle_after_jump(Pipeline *pipeline)
 {
   pipeline->decoded = decode_instr(pipeline->fetching);
   cycle_first(pipeline);
+  pipeline->current_state = Full;
 }
 
 void cycle_normal(Pipeline *pipeline)
 {
- pipeline->executing = pipeline->decoded;
- exec_instr(pipeline->emulator, &(pipeline->executing));
- cycle_after_jump(pipeline);
+  pipeline->executing = pipeline->decoded;
+  exec_instr(pipeline->emulator, &(pipeline->executing));
+  cycle_after_jump(pipeline);
+  if(pipeline->executing.type==Branch)
+  {
+    pipeline->current_state=Half;
+  }
 }
-
 
 void cycle_p(Pipeline *pipeline)
 {
@@ -50,6 +55,4 @@ void cycle_p(Pipeline *pipeline)
     /*Should never happen*/
     break;
   }
-
-
 }
