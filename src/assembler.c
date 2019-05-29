b86aa85d34d33551548a5511b3543ad2713b1afb
Nico D'Cotta <nicolas.dcotta18@imperial.ac.uk>
2019-05-29 12:12:20 +0100

    created the assembler

---
+++ src/assembler.c
@@ -0,0 +1,7 @@
+#include "assembler.h"
+
+#include "loader.h"
+
+void assemble(char *src_program_file, char *dest_bin_file)
+{
+}
---
+++ src/assembler.h
@@ -0,0 +1,8 @@
+#ifndef ASSEMBLER_H_
+#define ASSEMBLER_H
+
+#include <stdint.h>
+
+void assemble(char *src_program_file, char *dest_bin_file);
+
+#endif /* ASSEMBLER_H_ */
