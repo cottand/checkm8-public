f30760e7c5e6947563ef7d664cc52acdbab9b91d
Nico D'Cotta <nicolas.dcotta18@imperial.ac.uk>
2019-05-29 12:43:01 +0100

    Added some funcs to assembler

--- src/assembler.c
+++ src/assembler.c
@@ -4,4 +4,30 @@
 
 void assemble(char *src_program_file, char *dest_bin_file)
 {
+    /**
+     * TODO:
+     * load both files into arrays, src as read_only,
+     * Then assign those arrays to the struct Assembler
+     */
+}
+
+uint8_t get_src_byte(Assembler *assembler, uint16_t addr)
+{
+}
+void set_src_byte(Assembler *assembler, uint16_t addr, uint8_t val)
+{
+}
+
+uint8_t get_bin_byte(Assembler *assembler, uint16_t addr)
+{
+}
+void set_bin_byte(Assembler *assembler, uint16_t addr, uint8_t val)
+{
+}
+
+uint32_t get_bin_instr(Assembler *assembler, uint16_t addr)
+{
+}
+void set_bin_instr(Assembler *assembler, uint16_t addr, uint32_t val)
+{
 }
--- src/assembler.h
+++ src/assembler.h
@@ -1,8 +1,26 @@
 #ifndef ASSEMBLER_H_
 #define ASSEMBLER_H
 
+#define BIN_SIZE 1 //TO FIX
+#define PROG_SIZE 1
+
 #include <stdint.h>
 
+typedef struct s_Assembler
+{
+    uint8_t *bin[BIN_SIZE];
+    uint8_t *src[PROG_SIZE];
+} Assembler;
+
 void assemble(char *src_program_file, char *dest_bin_file);
+
+uint8_t get_src_byte(Assembler *assembler, uint16_t addr);
+void set_src_byte(Assembler *assembler, uint16_t addr, uint8_t val);
+
+uint8_t get_bin_byte(Assembler *assembler, uint16_t addr);
+void set_bin_byte(Assembler *assembler, uint16_t addr, uint8_t val);
+
+uint32_t get_bin_instr(Assembler *assembler, uint16_t addr);
+void set_bin_instr(Assembler *assembler, uint16_t addr, uint32_t val);
 
 #endif /* ASSEMBLER_H_ */
