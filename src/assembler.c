22c9fdef5af673426bcc2fb014f742bf0a00eaa1
Nico D'Cotta <nicolas.dcotta18@imperial.ac.uk>
2019-05-30 14:41:23 +0100

    further skecthed io loading

--- src/assembler.c
+++ src/assembler.c
@@ -1,40 +1,46 @@
 #include "assembler.h"
+#include <stdio.h>
+#include <string.h>
 
 #include "loader.h"
 
 void assemble(char *src_program_file, char *dest_bin_file)
 {
+    Assembler asse;
+    asse.src = read_src(src_program_file);
+
     /**
      * TODO:
      * load both files into arrays, src as read_only,
      * Then assign those arrays to the struct Assembler
      */
+
+    free(asse.src);
+    if (close_bin(dest_bin_file, *(asse.bin)) != 1)
+    {
+        printf("Save to bin unsuccsessful");
+    }
 }
 
-uint8_t *read_src(char *src_program_file)
+char **read_src(char *src_program_file)
 {
     //TODO
 }
 
-uint8_t *open_bin(char *dest_bin_file)
+
+/**
+ * Takes the path to the already opened bin file and writes 
+ * the array at bin_array to it. Returns 1 if everything went ok
+ */
+uint8_t load_to_bin(Assembler *asse, char *bin_path)
 {
     //TODO
-}
-
-int close_bin(char *src_program_file, uint8_t *src_array)
-{
-    //TODO
-}
-
-uint8_t get_src_byte(Assembler *assembler, uint16_t addr)
-{
-}
-void set_src_byte(Assembler *assembler, uint16_t addr, uint8_t val)
-{
+    return 1;
 }
 
 uint8_t get_bin_byte(Assembler *assembler, uint16_t addr)
 {
+    return 0;
 }
 void set_bin_byte(Assembler *assembler, uint16_t addr, uint8_t val)
 {
@@ -42,7 +48,14 @@ void set_bin_byte(Assembler *assembler, uint16_t addr, uint8_t val)
 
 uint32_t get_bin_instr(Assembler *assembler, uint16_t addr)
 {
+    return 0;
 }
+
 void set_bin_instr(Assembler *assembler, uint16_t addr, uint32_t val)
 {
+}
+
+void set_next_bin_instr(Assembler *assembler, uint32_t val){
+    set_bin_instr(assembler, assembler->current_writing_bin_address, val);
+    assembler->current_writing_bin_address += sizeof(uint32_t);
 }
--- src/assembler.h
+++ src/assembler.h
@@ -9,18 +9,16 @@
 typedef struct s_Assembler
 {
     uint8_t *bin;
-    uint8_t *src;
+    char *src;
+    uint16_t current_writing_bin_address;
 } Assembler;
 
 void assemble(char *src_program_file, char *dest_bin_file);
 
-uint8_t *read_src(char *src_program_file);
+char **read_src(char *src_program_file);
 
-uint8_t *open_bin(char *dest_bin_file);
-int close_bin(char *src_program_file, uint8_t *src_array);
+uint8_t load_to_bin(Assembler *asse, char *bin_path);
 
-uint8_t get_src_byte(Assembler *assembler, uint16_t addr);
-void set_src_byte(Assembler *assembler, uint16_t addr, uint8_t val);
 
 uint8_t get_bin_byte(Assembler *assembler, uint16_t addr);
 void set_bin_byte(Assembler *assembler, uint16_t addr, uint8_t val);
