a612dcf3dfba0d06b3356c0eb41e9e5e0dd4c506
Lancelot Blanchard <lancelotblanchard@hotmail.fr>
2019-05-31 22:30:52 +0100

    Updated Symbol_Table according to general style (using init and free methods)

--- src/symbol_table.c
+++ src/symbol_table.c
@@ -1,13 +1,23 @@
 #include <stdlib.h>
 #include "symbol_table.h"
 
-Symbol_Table *st_create(void)
+void st_init(Symbol_Table *st)
 {
-    return NULL;
+  st->max_size = DEFAULT_SYMBOL_TABLE_SIZE;
+  st->elements = 0;
+  st->items = calloc(DEFAULT_SYMBOL_TABLE_SIZE, sizeof(Table_Item *));
 }
 
-void st_delete(Symbol_Table *st)
+void st_free(Symbol_Table *st)
 {
+  int i;
+  for (i = 0; i < st->elements; i++)
+  {
+    Table_Item *item = st->items[i];
+    free(item->label);
+    free(item);
+  }
+  free(st);
 }
 
 void st_insert(Symbol_Table *st, char *label, int memory_addr)
@@ -16,7 +26,7 @@ void st_insert(Symbol_Table *st, char *label, int memory_addr)
 
 int st_search(Symbol_Table *st, char *label)
 {
-    return -1;
+  return -1;
 }
 
 void st_remove(Symbol_Table *st, char *label)
--- src/symbol_table.h
+++ src/symbol_table.h
@@ -16,8 +16,8 @@ typedef struct s_Symbol_Table
     Table_Item **items;
 } Symbol_Table;
 
-Symbol_Table *st_create(void);
-void st_delete(Symbol_Table *st);
+void st_init(Symbol_Table *st);
+void st_free(Symbol_Table *st);
 void st_insert(Symbol_Table *st, char *label, int memory_addr);
 int st_search(Symbol_Table *st, char *label);
 void st_remove(Symbol_Table *st, char *label);
