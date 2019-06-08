7c865ca515181b649edc870da4420f0440858a85
Nico D'Cotta <nicolas.dcotta18@imperial.ac.uk>
2019-06-08 02:29:46 +0100

    merged

--- src/gpio.s
+++ src/gpio.s
@@ -1,25 +1,27 @@
-ldr r7   =0x00FFFFFF  
-ldr r1   =0x00080000 
-ldr r3   =0x20200004 
-ldr r4   =0x20200028 
-ldr r5   =0x2020001C  
-orr [r3] [r3] =0x00001000
-and [r3] [r3] =0x11001111
+
+ldr r7   =0x000FFFFF
+ldr r1   =0x00080000
+ldr r3   =0x20200004
+ldr r4   =0x20200028
+ldr r5   =0x2020001C
+ldr [r3] =0x08000000
 clear:
-ldr [r4] r1	 
-mov r6   r7     
+ldr [r4] r1
+mov r6   r7
 loop1:
-sub r6	 r6	 #1	
+sub r6	 r6	 #1
 cmp r6   #0
 bne loop1
-ldr [r5] r1 
-mov r6   r7
+ldr [r5] r1 	
+mov r6   r7    
 loop2:
 sub r6	 r6	 #1
 cmp r6   #0
 bne loop2
 b clear
-
-
+andeq r0 r0 r0
+andeq r0 r0 r0
+andeq r0 r0 r0
+andeq r0 r0 r0
 
 
