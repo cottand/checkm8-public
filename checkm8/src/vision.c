5af19f4a41fedc4dc912e6a5563136f2530d2a90
Lancelot Blanchard <lancelotblanchard@hotmail.fr>
2019-06-12 21:59:49 +0100

    Attempts at detecting pieces

--- checkm8/src/vision.c
+++ checkm8/src/vision.c
@@ -52,12 +52,15 @@ CvRect **get_cells(float *topLeft, float *bottomRight)
   int cell_width = width / 8;
   int cell_height = height / 8;
 
+  float padding_x = 0.03 * width;
+  float padding_y = 0.03 * height;
+
   for(int i = 0; i < 8; i++)
   {
     cells[i] = malloc(8 * sizeof(CvRect));
     for (int j = 0; j < 8; j++)
     {
-      cells[i][j] = cvRect(topLeft[0]+j*cell_width, topLeft[1]+i*cell_height, cell_width, cell_height);
+      cells[i][j] = cvRect(topLeft[0]+j*cell_width + padding_x, topLeft[1]+i*cell_height + padding_y, cell_width - 2 *padding_x, cell_height - 2 *padding_y);
     }
   }
   return cells;
@@ -105,6 +108,26 @@ float get_cell_std_dev(IplImage *board, CvRect **cells, int row, int col)
   return std_dev.val[0];
 }
 
+bool is_cell_empty(IplImage *board, CvRect **cells, int row, int column, float std_dev_empty)
+{
+  float std_dev_cell = get_cell_std_dev(board, cells, row, column);
+
+  return fabs(std_dev_cell - std_dev_empty) <= 5;
+/*
+  CvMemStorage *storage = cvCreateMemStorage(0);
+
+  // This will store the lines
+  CvSeq *circles = NULL;
+
+  IplImage *image = get_cell(board, cells, row, column);
+  circles = cvHoughCircles(image, storage, CV_HOUGH_GRADIENT, 1, 100, 25, 27, 5, 20);
+
+  //printf("total : %d\n", circles->total);
+  //float *circle1 = (float *)cvGetSeqElem(circles, 0);
+  //cvCircle(image, cvPoint(circle1[0], circle1[1]), circle1[2], cvScalar(255, 0, 0, 0), 1, 8, 0);
+  return circles->total == 0;*/
+}
+
 int main()
 {
   // Loading image
@@ -132,7 +155,28 @@ int main()
 
   CvRect **cells = get_cells(topLeft, bottomRight);
 
-  cvShowImage("cells[7][7]", get_cell(board, cells, 7, 7));
+
+  float std_dev_empty = get_empty_std_dev(board, cells);
+  printf("std_dev_empty : %f\n", std_dev_empty);
+  printf("cell 7 7 : %f\n", get_cell_std_dev(board, cells, 4, 1));
+  printf("Cell[6][6] empty : %d\n", is_cell_empty(board, cells, 4, 1, std_dev_empty));
+
+  int count = 0;
+  for(int i = 0; i < 8; i++)
+  {
+    for (int j = 0; j < 8; j++)
+    {
+      bool empty = is_cell_empty(board, cells, i, j, std_dev_empty);
+      if(empty) {printf(".");}
+      else {printf("O"); count++;}
+      //cvShowImage("Cell", get_cell(board, cells, i, j));
+      //cvWaitKey(0);
+    }
+    printf("\n");
+  }
+  printf("%d\n", count);
+
+  cvShowImage("cells[7][7]", get_cell(board, cells, 4, 1));
   cvWaitKey(0);
   cvDestroyAllWindows();
 }
